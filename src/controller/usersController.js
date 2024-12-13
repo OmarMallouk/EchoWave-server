2d982cf8448437856b9a6c7145a21cef1871c472
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-13 23:53:59 +0200

    added users auth controller

---
+++ src/controller/usersController.js
@@ -0,0 +1,27 @@
+import { Users } from "../models/users.model";
+
+export const getUser = async (req,res) =>{
+    const id = req.params.id;
+
+    try{
+        if(id){
+        const user = await Users.findById(id);
+
+        if(!id){
+            res.status(404).send({message:"User not found!"});
+        }
+        return res.json(user);
+        }
+
+        const users = await Users.find()
+
+        return res.json(users)
+
+
+    }catch(error){
+        console.log(error.message);
+
+        return res.status(500).send({message:"Something went wrong :(("});
+        
+    }
+}

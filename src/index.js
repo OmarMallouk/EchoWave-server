9cf1c8275bb4a6b6d72fc9ee1ac8631d8400cedd
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-14 00:26:30 +0200

    added routes to index

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from "express";
 import connectToDatabase from "./base/connection.js";
+import userRoutes from "./routes/users.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
 app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 app.listen(8080, async () => {
     console.log("Server running on port 8080");
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { getUser, createUser, deleteUser } from "../controller/usersController.js";
-
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 
 const router = new Router();
 
-router.get("/:id?", getUser);
+router.get("/:id?",authMiddleware, getUser);
 router.post("/create", createUser);
 router.delete("/:id", deleteUser);
 

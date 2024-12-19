efbbeb84cd334b7ab16ff7df8e5ddb93c7533f10
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-20 01:02:40 +0200

    added more logic in the api

--- src/modules/originalityModule/originalityController.ts
+++ src/modules/originalityModule/originalityController.ts
@@ -16,5 +16,16 @@ export const getOriginality = async (req: Request, res: Response): Promise<any> 
             }
             return res.json(originality);
         }   
-    
+        const originalitys = await Originality.find();
+
+        return res.json(originalitys);
+
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+          console.log(error.message);
+        } else {
+          console.log("An unknown error occurred.");
+        }
+        return res.status(500).send({ message: "Something went wrong :(" });
+      }
     };

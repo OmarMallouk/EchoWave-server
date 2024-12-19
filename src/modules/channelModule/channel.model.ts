1f89b65b1fcde98bb7330e2ebc4f8ae9bdd688d4
OmarMallouk <mr.mallouk.om@gmail.com>
2024-12-20 01:28:14 +0200

    added more data in the model

--- src/modules/channelModule/channel.model.ts
+++ src/modules/channelModule/channel.model.ts
@@ -7,3 +7,30 @@ interface IChannel extends Document{
     producerId: Types.ObjectId,
     created_at: Date
 }
+
+const channelSchema = new Schema<IChannel>({
+    name:{
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true,
+    },
+    lyrics:{
+        type: Schema.Types.ObjectId,
+        ref: "Originality",
+        required: true,
+    },
+    producerId:{
+        type: Schema.Types.ObjectId,
+        ref: "Users",
+        required: true,
+    },
+    created_at:{
+        type: Date,
+        default: Date.now,
+    },
+
+});
+export const Channel = model<IChannel>("Channel", channelSchema);

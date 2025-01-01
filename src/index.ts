2d4e4122e0380637c8be0f2efdf30c00c0a73e78
OmarMallouk <mr.mallouk.om@gmail.com>
2025-01-01 23:45:25 +0200

    added cors to index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import cors from "cors";
+
 import connectToDatabase from "./base/connection";
 import userRoutes from "./modules/userModule/users.routes";
 import authRoutes from "./modules/authModule/auth.routes";
 
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET","POST","PUT","DELETE"],
+    credentials: true
+}))
 
 app.use(express.json());
 app.use("/auth", authRoutes);

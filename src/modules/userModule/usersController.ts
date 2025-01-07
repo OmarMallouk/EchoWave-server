791568973a9b3b252bd89b530c1aa3ea1f69f1de
OmarMallouk <mr.mallouk.om@gmail.com>
2025-01-07 03:09:50 +0200

    made changes to user controller

--- src/modules/userModule/usersController.ts
+++ src/modules/userModule/usersController.ts
@@ -6,17 +6,21 @@ export const getUser = async (req: Request,res: Response): Promise<any> =>{
 
     try{
         if(id){
-        const user = await Users.findById(id);
+        const user = await Users.findById(id).populate({
+          path: 'lyrics', strictPopulate: false
+      });
 
         if(!id){
             res.status(404).send({message:"User not found!"});
         }
         return res.json(user);
-        }
+        };
 
-        const users = await Users.find()
+        const users = await Users.find().populate({
+          path: 'lyrics', strictPopulate: false
+      });
 
-        return res.json(users)
+        return res.json(users);
 
 
     } catch (error: unknown) {
@@ -31,16 +35,27 @@ export const getUser = async (req: Request,res: Response): Promise<any> =>{
 
 
 export const createUser = async (req: Request, res: Response): Promise<any> => {
-    const { username, password, email } = req.body;
+    const { username, password, role, email, channelName  } = req.body;
   
     try {
-      const user = await Users.create({
-        username,
-        password,
-        email,
-      });
+
+      if (role === "song_producer" && !channelName) {
+        return res.status(400).json({ message: "Channel name is required for song producers." });
+    }
+
+    const user = await Users.create({
+      username,
+      password,
+      email,
+      role: role || "user",
+      channelName: role === "song_producer" ? channelName : undefined,
+    });
   
-      return res.json(user);
+    return res.status(201).json({
+      message: "User created successfully.",
+      user,
+  });
+
     } catch (error: unknown) {
         if(error instanceof Error){
             console.error(error.message);
